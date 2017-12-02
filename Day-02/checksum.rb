fe41bae1ecc364cf7a485528772167abffe9447e
SahinU88 <sahin@flyingfalcon.io>
2017-12-02 14:14:56 +0100

    Optimize a little

--- Day-02/checksum.rb
+++ Day-02/checksum.rb
@@ -4,7 +4,8 @@ input = File.open( 'input.txt' ).read
 input.gsub!(/\r\n?/, "\n")
 input.each_line do |line|
     numbers = line.split( '|' ).map { |s| s.to_i }
-    checksum += ( numbers.minmax.last - numbers.minmax.first )
+    minmax = numbers.minmax
+    checksum += ( minmax.last - minmax.first )
 end
 
 puts checksum
