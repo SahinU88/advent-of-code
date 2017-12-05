ea37625a8a7120eb7f28237b0e327344c1b2894c
SahinU88 <sahin@flyingfalcon.io>
2017-12-05 15:34:50 +0100

    Add task 02 as seperate file

---
+++ Day-04/README.md
@@ -0,0 +1,11 @@
+# Passphrases
+
+Did this challenge with the `tightenco/collect` composer package. Used `::macro` the first time.
+
+## Task One
+
++ macro-function for `duplicates`
+
+## Task Two
+
++ macro-function for `anagrams`
--- Day-04/task-02.php
+++ Day-04/task-02.php
@@ -6,6 +6,11 @@ require_once 'vendor/autoload.php';
 
 $passphrases = file_get_contents( 'passphrases.txt' );
 
+function is_anagram( $a, $b )
+{
+    return ( count_chars( $a, 1 ) == count_chars( $b, 1 ) );
+}
+
 Collection::macro( 'duplicates', function(){
     return collect( array_count_values( $this->toArray() ) )
         ->filter( function( $value, $key ){
@@ -13,11 +18,19 @@ Collection::macro( 'duplicates', function(){
         });
 });
 
+Collection::macro( 'anagrams', function(){
+    return $this->crossJoin( $this )->filter( function( $pair ){
+        [ $a, $b ] = $pair;
+        
+        return ( $a !== $b ) && is_anagram( $a, $b );
+    });
+});
+
 
 echo collect( explode( PHP_EOL, $passphrases ) )
         ->reject( function( $row ){
             $words = collect( explode( ' ', $row ) );
             
-            return $words->duplicates()->isNotEmpty();
+            return $words->duplicates()->isNotEmpty() || $words->anagrams()->isNotEmpty();
         })
         ->count();
