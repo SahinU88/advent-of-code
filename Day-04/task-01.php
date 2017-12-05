0cd85a9d80f8af9f1f0b285601be86b19e72e845
SahinU88 <sahin@flyingfalcon.io>
2017-12-05 14:43:28 +0100

    Solution with a Macro solution in Collections

--- Day-04/task-01.php
+++ Day-04/task-01.php
@@ -1,13 +1,22 @@
 <?php
 
+use Illuminate\Support\Collection;
+
 require_once 'vendor/autoload.php';
 
 $passphrases = file_get_contents( 'passphrases.txt' );
 
+Collection::macro( 'hasDuplicates', function(){
+    return collect( array_count_values( $this->toArray() ) )
+        ->filter( function( $value, $key ){
+            return $value > 1;
+        })
+        ->isNotEmpty();
+});
+
+
 echo collect( explode( PHP_EOL, $passphrases ) )
         ->reject( function( $row ){
-            $words = collect( explode( ' ', $row ) );
-        
-            return ( $words->unique()->count() < $words->count() );
+            return collect( explode( ' ', $row ) )->hasDuplicates();
         })
         ->count();
